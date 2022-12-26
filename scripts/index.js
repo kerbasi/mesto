ce0e3fecb910b15cd5530d4fcf2eb06d84d572c2
Kirill Novikov <kirill.novikov@gmail.com>
2022-12-26 09:22:53 +0300

    fix js review issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,32 @@
 ﻿const editButton = document.querySelector(".profile__edit-button");
-const plusButton = document.querySelector(".profile__add-button");
 const popup = document.querySelector(".popup");
 const saveButton = popup.querySelector(".popup__button");
 const quitButton = popup.querySelector(".popup__cross");
-const form = document.forms[0];
+const form = document.querySelector(".popup__form");
+const formName = form.querySelector(".popup__input_type_name");
+const formAbout = form.querySelector(".popup__input_type_about");
 const profileName = document.querySelector(".profile__info-title");
 const profileAbout = document.querySelector(".profile__info-subtitle");
 
-editButton.addEventListener("click", () => {
+editButton.addEventListener("click", openForm);
+
+quitButton.addEventListener("click", closeForm);
+
+form.addEventListener("submit", saveForm);
+
+function openForm() {
   popup.classList.add("popup_opened");
-  window.addEventListener("keydown", escHandler);
-  form.name.value = profileName.textContent;
-  form.about.value = profileAbout.textContent;
-});
+  formName.value = profileName.textContent;
+  formAbout.value = profileAbout.textContent;
+}
 
-quitButton.addEventListener("click", () => {
+function closeForm() {
   popup.classList.remove("popup_opened");
-  window.removeEventListener("keydown", escHandler);
-});
+}
 
-saveButton.addEventListener("click", (event) => {
+function saveForm(event) {
   event.preventDefault();
-  profileName.textContent = form.name.value;
-  profileAbout.textContent = form.about.value;
+  profileName.textContent = formName.value;
+  profileAbout.textContent = formAbout.value;
   popup.classList.remove("popup_opened");
-});
-
-function escHandler(e) {
-  if (e.key === "Escape") {
-    popup.classList.remove("popup_opened");
-    window.removeEventListener("keydown", escHandler);
-  }
 }
