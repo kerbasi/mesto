a626a703990dc44ca4d0b21570fa50500da66539
Kirill <kirill.novikov@gmail.com>
2023-03-26 19:13:27 +0300

    Add renderItems to Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,6 +6,12 @@
   addItem(item) {
     this._container.prepend(item);
   }
+
+  renderItems(renderedItems) {
+    renderedItems.reverse().forEach((item) => {
+      this.addItem(this._renderer(item));
+    });
+  }
 }
 
 export default Section;
